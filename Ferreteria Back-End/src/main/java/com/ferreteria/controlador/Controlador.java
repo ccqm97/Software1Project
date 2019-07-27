188f7d6f3331cf6198ca802ca0d4c34e78c7a02a
Oscar Bosigas <oscar.bosigas@uptc.edu.co>
2019-07-27 16:38:40 -0500

    Controlador

--- Ferreteria Back-End/src/main/java/com/ferreteria/controlador/Controlador.java
+++ Ferreteria Back-End/src/main/java/com/ferreteria/controlador/Controlador.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author oscar
  */
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200/home", maxAge = 3600)
 @RestController
 @RequestMapping({"/producto"})
 public class Controlador {
