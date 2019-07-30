903e61b4bdf3f13457b8fa768e10a7fc03b7ed72
Oscar Bosigas <oscar.bosigas@uptc.edu.co>
2019-07-30 16:28:18 -0500

    structure

--- Ferreteria Front-End/src/app/app-routing.module.ts
+++ Ferreteria Front-End/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
+import { StructureComponent } from './components/structure/structure.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'structure', component: StructureComponent}
 ];
 
 
